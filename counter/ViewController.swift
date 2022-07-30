6512183e5a06fb57ff5c6ab67e2993cdae1fd8b6
khitrind <32079217+khitrind@users.noreply.github.com>
2022-07-31 00:22:55 +0300

    Sprint-3: Added counter - UILabel and UIButton  (#1)
    * Sprint-3: Added view at storyboard
    
    * Sprint-3: Added counter and buttons
    
    * Sprint-3: fixed constraints
    
    * Sprint-3: StackView

--- counter/ViewController.swift
+++ counter/ViewController.swift
@@ -8,10 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var counter: Int = 0
+    
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    @IBAction func reset() {
+        counter = 0
+        updateLabel()
+    }
+    @IBAction func decreaseButton() {
+        counter = max(counter - 1, 0)
+        updateLabel()
+    }
+    @IBAction func increaseCounter(_ sender: Any) {
+        counter += 1
+        updateLabel()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateLabel()
         // Do any additional setup after loading the view.
+    }
+    
+    func updateLabel() {
+        counterLabel.text = "Значение счётчика: \(counter)"
     }
 
 
